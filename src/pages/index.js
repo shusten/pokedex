8b679b85ffe5ffd21efb5c0054425c31218a3647
Luis Villas-Boas <shusten@hotmail.com>
2023-10-11 17:58:55 -0300

    :construction: Adiciona requisição para api pokemon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,37 @@
-import Head from 'next/head'
+import Image from "next/image";
 
+export async function getStaticProps() {
+  const maxPokemons = 200;
+  const API = 'https://pokeapi.co/api/v2/pokemon/';
 
-export default function Home() {
-  return <h1>Pokedex</h1>
+  const res   = await fetch(`${API}?limit=${maxPokemons}`);
+  const data  = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
+  return (
+    <>
+      <div>
+        <h1>Pokedex</h1>
+        <Image src="/pokeball.png" width="60" height="60" alt="pokeball"/>
+      </div>
+      <div>
+        {
+          pokemons.map((pokemon) => (
+            <p key={pokemon.id}>{pokemon.name}</p>
+          ))
+        }
+      </div>
+    </>
+  )
 }
